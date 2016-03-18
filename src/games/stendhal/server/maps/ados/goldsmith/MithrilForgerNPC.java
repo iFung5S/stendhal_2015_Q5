31a80f525b69230450e1290d0c84aed6eb865a73
Hikmat Hajiyev <>
2016-03-18 11:14:07 +0000

    changed Pedinghaus so that he now creates a stealth ring in exchange gold and mithril

--- src/games/stendhal/server/maps/ados/goldsmith/MithrilForgerNPC.java
+++ src/games/stendhal/server/maps/ados/goldsmith/MithrilForgerNPC.java
@@ -16,12 +16,14 @@ import games.stendhal.common.Direction;
 import games.stendhal.server.core.config.ZoneConfigurator;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.entity.npc.SpeakerNPC;
-import games.stendhal.server.entity.npc.behaviour.adder.ProducerAdder;
-import games.stendhal.server.entity.npc.behaviour.impl.ProducerBehaviour;
+import games.stendhal.server.entity.npc.behaviour.adder.MultiProducerAdder;
+import games.stendhal.server.entity.npc.behaviour.impl.MultiProducerBehaviour;
 
 import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * Ados MithrilForger (Inside / Level 0).
@@ -56,22 +58,60 @@ public class MithrilForgerNPC implements ZoneConfigurator {
 				addGoodbye("Bye.");
 
 				// Pedinghaus makes mithril if you bring him mithril nugget and wood
-				final Map<String, Integer> requiredResources = new TreeMap<String, Integer>();	
-				requiredResources.put("wood", 20);
-				requiredResources.put("mithril nugget", 1);
+				final HashSet<String> productsNames = new HashSet<String>();
+                productsNames.add("mithril");
+                productsNames.add("stealth ring");
+			    final HashMap<String, Map<String, Integer>> requiredResources = new HashMap<String, Map<String, Integer>>();	
 
-				final ProducerBehaviour behaviour = new ProducerBehaviour("Pedinghaus_cast_mithril",
-						"cast", "mithril bar", requiredResources, 18 * 60);
+			    final Map<String, Integer> requiredResourcesForMithril = new TreeMap<String, Integer>();	
+				requiredResourcesForMithril.put("wood", 20);
+				requiredResourcesForMithril.put("mithril nugget", 1);
 
-				new ProducerAdder().addProducer(this, behaviour,
-				        "Greetings. I sense you may be interested in mithril. If you desire me to #cast you a #'mithril bar', just say the word.");
+				final Map<String, Integer> requiredResourcesForRing = new TreeMap<String, Integer>();	
+				
+				// Pedinghaus makes stealth ring you bring him mithril and gold nugget
+				requiredResourcesForRing.put("gold nugget", 1);
+				requiredResourcesForRing.put("mithril nugget", 1);
+				
+				// for testing
+				// requiredResourcesForRing.put("cheese", 1);
+				
+				requiredResources.put("mithril", requiredResourcesForMithril);
+				requiredResources.put("stealth ring", requiredResourcesForRing);
+
+				final HashMap<String, Integer> productionTimesPerProduct = new HashMap<String, Integer>();
+				productionTimesPerProduct.put("mithril", 18 * 60);
+				productionTimesPerProduct.put("stealth ring", 25 * 60);
+        
+
+				final HashMap<String, Boolean> boundMap = new HashMap<String, Boolean>();
+				boundMap.put("mithril", true);
+				boundMap.put("stealth ring", true);
+				
+  			    final MultiProducerBehaviour behaviour = new MultiProducerBehaviour(
+		          "Pedinghaus_cast_mithrilandring",
+		          "make",
+		          productsNames,
+		          requiredResources,
+		          productionTimesPerProduct,
+		          boundMap
+		          );
+
+				new MultiProducerAdder().addMultiProducer(this, behaviour,
+				        "Greetings. I sense you may be interested in mithril. If you desire me to #make you a #'mithril bar' or #stealth #ring just say the word.");
 				addReply("wood",
 		        		"The wood is for the fire. I hope you collect yours from the forest, and not the barbaric practise of killing ents.");
 				addReply(Arrays.asList("mithril ore", "mithril nugget"),
 				        "Nowadays these rare nuggets are only likely to be found in the Ados mountains. I have no idea if that area is still civilised...");
+				addReply(Arrays.asList("stealth", "stealth ring", "ring"),
+				        "One ring to rule them all, one ring to find them all, one ring to bring them and in the darkness bind them...");
 				addReply(Arrays.asList("mithril bar", "mithril", "bar"),
 				        "Mithril is an incredibly valuable commodity, as it makes armor of astounding strength, yet remains featherlight. Guard any mithril stash you own with great care.");
 			}
+
+
+			
+
 		};
 
 		forger.setEntityClass("mithrilforgernpc");
