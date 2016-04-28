56063625f67daea843c62ab0099645059118e94c
Vladislav Havalyov <>
2016-04-28 16:24:14 +0100

    #15 Converted class CoalForHaunchy quest to fit the new system. Have already played around using the server

--- src/games/stendhal/server/maps/quests/CoalForHaunchy.java
+++ src/games/stendhal/server/maps/quests/CoalForHaunchy.java
@@ -12,39 +12,18 @@
  ***************************************************************************/
 package games.stendhal.server.maps.quests;
 
-import games.stendhal.common.Rand;
-import games.stendhal.common.grammar.Grammar;
-import games.stendhal.common.parser.Sentence;
-import games.stendhal.server.entity.npc.ChatAction;
-import games.stendhal.server.entity.npc.ConversationPhrases;
-import games.stendhal.server.entity.npc.ConversationStates;
-import games.stendhal.server.entity.npc.EventRaiser;
 import games.stendhal.server.entity.npc.SpeakerNPC;
-import games.stendhal.server.entity.npc.action.DropItemAction;
-import games.stendhal.server.entity.npc.action.EquipItemAction;
-import games.stendhal.server.entity.npc.action.IncreaseKarmaAction;
-import games.stendhal.server.entity.npc.action.IncreaseXPAction;
-import games.stendhal.server.entity.npc.action.MultipleActions;
-import games.stendhal.server.entity.npc.action.SayTimeRemainingAction;
-import games.stendhal.server.entity.npc.action.SetQuestAndModifyKarmaAction;
-import games.stendhal.server.entity.npc.condition.AndCondition;
-import games.stendhal.server.entity.npc.condition.NotCondition;
-import games.stendhal.server.entity.npc.condition.PlayerHasItemWithHimCondition;
-import games.stendhal.server.entity.npc.condition.QuestCompletedCondition;
-import games.stendhal.server.entity.npc.condition.QuestInStateCondition;
-import games.stendhal.server.entity.npc.condition.QuestNotInStateCondition;
-import games.stendhal.server.entity.npc.condition.QuestNotStartedCondition;
-import games.stendhal.server.entity.npc.condition.QuestStateStartsWithCondition;
-import games.stendhal.server.entity.npc.condition.TimePassedCondition;
-import games.stendhal.server.entity.player.Player;
-import games.stendhal.server.maps.Region;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import games.stendhal.server.maps.Region;
+import games.stendhal.server.maps.quests.logic.AbstractQuestState;
+import games.stendhal.server.maps.quests.logic.StateAskForQuest;
+import games.stendhal.server.maps.quests.logic.StateCollectItems;
+
+import java.util.LinkedList;
 import java.util.List;
 
 /**
- * QUEST: Coal for Haunchy
+ * QUEST: Coal for Haunchy // Converted version \\
  *
  * PARTICIPANTS:
  * <ul>
@@ -71,191 +50,102 @@ import java.util.List;
  * <li>You can repeat it each 2 days.</li>
  * </ul>
  * 
- * @author Vanessa Julius and storyteller
+ * @author Vladislav Havalyov
  */
-public class CoalForHaunchy extends AbstractQuest {
+
+public class CoalForHaunchy extends StatedAbstractQuest 
+{
 
 	private static final String QUEST_SLOT = "coal_for_haunchy";
 
 	// The delay between repeating quests is 48 hours or 2880 minutes
 	private static final int REQUIRED_MINUTES = 2880;
+	
+	// All the name of the states in the quest
+	public static final String STATE_START = "start_state";
+	public static final String STATE_NEEDED_ITEMS = "coal=25;";
+	public static final String STATE_COMPLETE = "done";	
+	
+	
+	// Offer quest steps
+	private static final String HAUNCHY_WANT_COAL = "I cannot use wood for this huge BBQ. To keep the heat I need some really old stone coal but there isn't much left. The problem is, that I can't fetch it myself because my steaks would burn then so I have to stay here. Can you bring me 25 pieces of #coal for my BBQ please?";
+	private static final String HAUNCHY_RECEIVE_COAL = "I can go on with grilling my tasty steaks now! Thank you!";
+	private static final String HAUNCHY_ASK_FOR_COAL_AGAIN = "The last coal you brought me is mostly gone again. Will you bring me some more?";
+	private static final String HAUNCHY_DOES_NOT_NEED_MORE_COAL_FOR_NOW = "The coal amount behind my counter is still high enough. I will not need more at the moment";
+	private static final String PLAYER_AGREE_GET_COAL = "Thank you! If you have found 25 pieces, say #coal to me so I know you have it. I'll be sure to give you a nice and tasty reward.";
+	private static final String PLAYER_DOES_NOT_AGREE_GET_COAL = "Oh, never mind. I thought you love BBQs like I do. Bye then.";
+	
+	
+	// The list that stores all the states
+	private  List<AbstractQuestState> statesList = new LinkedList<AbstractQuestState>();
+	
 
-	private void offerQuestStep() {
-		final SpeakerNPC npc = npcs.get("Haunchy Meatoch");
-		
-		// player says quest when he has not ever done the quest before (rejected or just new)
-		npc.add(ConversationStates.ATTENDING,
-				ConversationPhrases.QUEST_MESSAGES, 
-				new QuestNotStartedCondition(QUEST_SLOT),
-				ConversationStates.QUEST_OFFERED, 
-				"I cannot use wood for this huge BBQ. To keep the heat I need some really old stone coal but there isn't much left. The problem is, that I can't fetch it myself because my steaks would burn then so I have to stay here. Can you bring me 25 pieces of #coal for my BBQ please?",
-				null);
-
-		npc.add(
-				ConversationStates.QUEST_OFFERED,
-				Arrays.asList("coal"),
-				null,
-				ConversationStates.QUEST_OFFERED,
-				"Coal isn't easy to find. You normally can find it somewhere in the ground but perhaps you are lucky and find some in the old Semos Mine tunnels...",
-				null);
-
-        // player has completed the quest (doesn't happen here)
-		npc.add(ConversationStates.ATTENDING,
-				ConversationPhrases.QUEST_MESSAGES,
-				new QuestCompletedCondition(QUEST_SLOT),
-				ConversationStates.ATTENDING,
-				"I can go on with grilling my tasty steaks now! Thank you!",
-				null);
-
-		// player asks about quest which he has done already and he is allowed to repeat it
-		npc.add(ConversationStates.ATTENDING,
-				ConversationPhrases.QUEST_MESSAGES,
-				new AndCondition(new TimePassedCondition(QUEST_SLOT, 1, REQUIRED_MINUTES), new QuestStateStartsWithCondition(QUEST_SLOT, "waiting;")),
-				ConversationStates.QUEST_OFFERED,
-				"The last coal you brought me is mostly gone again. Will you bring me some more?",
-				null);
-		
-		// player asks about quest which he has done already but it is not time to repeat it
-		npc.add(ConversationStates.ATTENDING,
-				ConversationPhrases.QUEST_MESSAGES,
-				new AndCondition(new NotCondition(new TimePassedCondition(QUEST_SLOT, 1, REQUIRED_MINUTES)), new QuestStateStartsWithCondition(QUEST_SLOT, "waiting;")),
-				ConversationStates.ATTENDING,
-				null,
-				new SayTimeRemainingAction(QUEST_SLOT, 1, REQUIRED_MINUTES, "The coal amount behind my counter is still high enough. I will not need more for"));
-
-		// Player agrees to get the coal, increase 5 karma
-		npc.add(ConversationStates.QUEST_OFFERED,
-				ConversationPhrases.YES_MESSAGES, null,
-				ConversationStates.ATTENDING,
-				"Thank you! If you have found 25 pieces, say #coal to me so I know you have it. I'll be sure to give you a nice and tasty reward.",
-				new SetQuestAndModifyKarmaAction(QUEST_SLOT, "start", 5));
-
-		// Player says no, they've lost karma.
-		npc.add(ConversationStates.QUEST_OFFERED,
-				ConversationPhrases.NO_MESSAGES, null, ConversationStates.IDLE,
-				"Oh, never mind. I thought you love BBQs like I do. Bye then.",
-				new SetQuestAndModifyKarmaAction(QUEST_SLOT, "rejected", -10.0));
-	}
-
-	/*
-	 * Get Coal Step :
-	 * Players will get some coal in Semos Mine and with buying some from other players.
-	 * 
-	 */
-	private void bringCoalStep() {
-		final SpeakerNPC npc = npcs.get("Haunchy Meatoch");
-		
-		final List<String> triggers = new ArrayList<String>();
-		triggers.add("coal");
-		triggers.add("stone coal");
-		triggers.addAll(ConversationPhrases.QUEST_MESSAGES);
-
-		// player asks about quest or says coal when they are supposed to bring some coal and they have it
-		npc.add(
-				ConversationStates.ATTENDING, triggers,
-				new AndCondition(new QuestInStateCondition(QUEST_SLOT, "start"), new PlayerHasItemWithHimCondition("coal",25)),
-				ConversationStates.ATTENDING, 
-				null,
-				new MultipleActions(
-						new DropItemAction("coal",25), 
-						new IncreaseXPAction(200),
-						new IncreaseKarmaAction(20),
-						new ChatAction() {
-							@Override
-							public void fire(final Player player,
-									final Sentence sentence,
-									final EventRaiser npc) {
-								int grilledsteakAmount = Rand.rand(4) + 1;
-								new EquipItemAction("grilled steak", grilledsteakAmount, true).fire(player, sentence, npc);
-								npc.say("Thank you!! Take " + Grammar.thisthese(grilledsteakAmount) + " " +
-										Grammar.quantityNumberStrNoun(grilledsteakAmount, "grilled steak") + " from my grill!");
-								new SetQuestAndModifyKarmaAction(getSlotName(), "waiting;" 
-										+ System.currentTimeMillis(), 10.0).fire(player, sentence, npc);
-							}
-						}));
-
-		// player asks about quest or says coal when they are supposed to bring some coal and they don't have it
-		npc.add(
-				ConversationStates.ATTENDING, triggers,
-				new AndCondition(new QuestInStateCondition(QUEST_SLOT, "start"), new NotCondition(new PlayerHasItemWithHimCondition("coal",25))),
-				ConversationStates.ATTENDING,
-				"You don't have the coal amount which I need yet. Go and pick some more pieces up, please.",
-				null);
-		
-		npc.add(
-				ConversationStates.ATTENDING, 
-				Arrays.asList("coal","stone coal"),
-				new QuestNotInStateCondition(QUEST_SLOT,"start"),
-				ConversationStates.ATTENDING,
-				"Sometime you could do me a #favour ...", null);
-	}
 
 	@Override
-	public void addToWorld() {
+	public void addToWorld()
+	{
 		fillQuestInfo(
 				"Coal for Haunchy",
 				"Haunchy Meatoch is afraid of his BBQ grillfire. Will his coal last till the steaks are ready or will he need some more?",
 				true);
-		offerQuestStep();
-		bringCoalStep();
+		SpeakerNPC npc = npcs.get("Haunchy Meatoch");
+	
+		// Set the quest to be repeatable
+		setRepeatable(REQUIRED_MINUTES);
+	
+		
+		// Create the start state
+		StateAskForQuest startState = new StateAskForQuest(npc, STATE_START, STATE_NEEDED_ITEMS, QUEST_SLOT, PLAYER_AGREE_GET_COAL,
+															REQUIRED_MINUTES);
+		startState.setPurposeQuest(HAUNCHY_WANT_COAL);
+		startState.setPurposeAgain(HAUNCHY_ASK_FOR_COAL_AGAIN);
+		startState.setCooldownReply(HAUNCHY_DOES_NOT_NEED_MORE_COAL_FOR_NOW);
+		startState.setNoToQuest(PLAYER_DOES_NOT_AGREE_GET_COAL);
+		statesList.add(startState);
+				
+		// Create the Collecting items state
+		StateCollectItems collectingState = new StateCollectItems(npc, STATE_NEEDED_ITEMS, STATE_COMPLETE, QUEST_SLOT);
+		collectingState.setFinishReply(HAUNCHY_RECEIVE_COAL);
+		
+		collectingState.setXP(200);
+		collectingState.setKarmaAction(25);
+		collectingState.addRewardItem("grilled steak", 2);	
+		statesList.add(collectingState);
+	
+			
+		// Add all the states into the World
+		StatedAbstractQuest.addAllState(statesList);
+		
 	}
 
-
+	
 	@Override
-	public List<String> getHistory(final Player player) {
-		final List<String> res = new ArrayList<String>();
-		if (!player.hasQuest(QUEST_SLOT)) {
-			return res;
-		}
-		res.add("Haunchy Meatoch welcomed me to the Ados market.");
-		final String questState = player.getQuest(QUEST_SLOT);
-		if ("rejected".equals(questState)) {
-			res.add("He asked me to fetch him some pieces of coal but I don't have time to collect some.");
-		}
-		if (player.isQuestInState(QUEST_SLOT, "start") || isCompleted(player)) {
-			res.add("The BBQ grill-heat is low and I promised Haunchy to help him out with 25 pieces of coal.");
-		}
-		if ("start".equals(questState) && player.isEquipped("coal",25) || isCompleted(player)) {
-			res.add("I found 25 pieces of coal for the Haunchy and think he will be happy.");
-		}
-		if (isCompleted(player)) {
-			if (isRepeatable(player)) {
-				res.add("I took 25 pieces of coal to the Haunchy, but I'd bet his amount is low again and needs more. Maybe I'll get more grilled tasty steaks.");
-			} else {
-				res.add("Haunchy Meatoch was really happy when I gave him the coal, he has enough for now. He gave me some of the best steaks which I ever ate!");
-			}			
-		}
-		return res;
-	}
-
-	@Override
-	public String getSlotName() {
+	public String getSlotName()
+	{
 		return QUEST_SLOT;
 	}
 
 	@Override
-	public String getName() {
+	public String getName() 
+	{
 		return "CoalForHaunchy";
 	}
 
 	@Override
-	public boolean isRepeatable(final Player player) {
-		return new AndCondition(new QuestStateStartsWithCondition(QUEST_SLOT,"waiting;"),
-				new TimePassedCondition(QUEST_SLOT, 1, REQUIRED_MINUTES)).fire(player,null, null);
-	}
-
-	@Override
-	public boolean isCompleted(final Player player) {
-		return new QuestStateStartsWithCondition(QUEST_SLOT,"waiting;").fire(player, null, null);
-	}
+	public List<AbstractQuestState> getStateList(){
+		return statesList;
+	}	
+	
 	
 	@Override
-	public String getRegion() {
+	public String getRegion()
+	{
 		return Region.ADOS_CITY;
 	}
 
 	@Override
-	public String getNPCName() {
+	public String getNPCName()
+	{
 		return "Haunchy Meatoch";
 	}
 }
