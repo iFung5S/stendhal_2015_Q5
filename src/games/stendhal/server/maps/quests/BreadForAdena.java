98fa83c9aad10680b62d15eca6e212341142c6d5
Roger Milnes <>
2016-04-29 12:50:44 +0100

    issue of Adena not accepting bread fixed \n the BreadForAdena quest can now be completed - when you get the two breads Adena will now accept them

--- src/games/stendhal/server/maps/quests/BreadForAdena.java
+++ src/games/stendhal/server/maps/quests/BreadForAdena.java
@@ -8,7 +8,6 @@ import games.stendhal.server.maps.Region;
 import games.stendhal.server.maps.quests.logic.AbstractQuestState;
 import games.stendhal.server.maps.quests.logic.StateAskForQuest;
 import games.stendhal.server.maps.quests.logic.StateCollectItems;
-import games.stendhal.server.maps.quests.logic.StatePaperChase;
 /**
  * 
  * @author Suhasini Suresh
@@ -23,18 +22,15 @@ public class BreadForAdena extends StatedAbstractQuest {
     
     // All the name of the states in the quest
    	public static final String STATE_START = "start_state";
-   	public static final String STATE_NEEDED_ITEMS = "bread = 2;";
-   	public static final String STATE_BAKER = "baker";
+   	public static final String STATE_NEEDED_ITEMS = "bread=2";
    	public static final String STATE_DONE = "done";	
    	
     // Cool down time before the quest become repeatable
    	private static final int REQUIRED_MINUTES = 1;
    	
-   	public static final String PW_ADENA = "done";
    	
    // Replies from each NPC at the end of each state
    	private static final String QUEST_DETAIL = "Please bring me two breads from Erna the baker.";
-   	private static final String BAKER_REPLY = "Here are all the bread that you need";
    	private static final String ADENA_REPLY = "Thank you for your help young one! Let me give you some apples as a reward";
    	
    // The list that stores all the states
@@ -73,18 +69,12 @@ public class BreadForAdena extends StatedAbstractQuest {
 		statesList.add(startState);
 		
 		// Create the Collecting items state
-		StateCollectItems collectingState = new StateCollectItems(npc1, STATE_NEEDED_ITEMS, STATE_BAKER, QUEST_SLOT);
-		collectingState.setFinishReply(BAKER_REPLY);
-		statesList.add(collectingState);
-	
-	
-		// Give items state
-		StatePaperChase paperChase = new StatePaperChase(npc1, STATE_BAKER, STATE_DONE, 
-																QUEST_SLOT, PW_ADENA, ADENA_REPLY);
-				// End of the quest reward (Add to the final state)
-		paperChase.setXP(300);
-	   paperChase.addRewardItem("apples", 10);
-		statesList.add(paperChase);		    	
+		StateCollectItems collectingState = new StateCollectItems(npc1, STATE_NEEDED_ITEMS, STATE_DONE, QUEST_SLOT);
+		collectingState.setFinishReply(ADENA_REPLY);
+		// End of the quest reward (Add to the final state)
+		collectingState.setXP(300);
+		collectingState.addRewardItem("apples", 10);
+		statesList.add(collectingState);	    	
 
 		// Add all the states into the World
 		StatedAbstractQuest.addAllState(statesList);
